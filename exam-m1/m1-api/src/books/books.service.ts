9eb7dbd95c79792ba65772c2e02726b466a50964
florianVit <72154681+florianVit@users.noreply.github.com>
2024-11-08 16:30:23 +0100

    commit de départ
    mise en commun pour julien

--- exam-m1/m1-api/src/books/books.service.ts
+++ exam-m1/m1-api/src/books/books.service.ts
@@ -3,14 +3,14 @@ import { CreateBookDto } from './dto/create-book.dto';
 
 @Injectable()
 export class BooksService {
-  private books = []; // Simuler une base de données
+  private books = []; //base de données
 
-  // Méthode pour récupérer tous les livres
+  //Récupérer tous les livres
   findAll() {
     return this.books;
   }
 
-  // Méthode pour trouver un livre par ID
+  //Trouver un livre par ID
   findOne(id: string) {
     const book = this.books.find(b => b.id === id);
     if (!book) {
@@ -19,14 +19,14 @@ export class BooksService {
     return book;
   }
 
-  // Méthode pour créer un livre
+  //Créer un livre
   create(createBookDto: CreateBookDto) {
     const newBook = { id: Date.now().toString(), ...createBookDto };
     this.books.push(newBook);
     return newBook;
   }
 
-  // Méthode pour supprimer un livre
+  //Supprimer un livre
   remove(id: string) {
     const bookIndex = this.books.findIndex(b => b.id === id);
     if (bookIndex === -1) {
--- exam-m1/m1-api/src/books/dto/create-book.dto.ts
+++ exam-m1/m1-api/src/books/dto/create-book.dto.ts
@@ -3,12 +3,12 @@ import { IsString, IsNotEmpty, IsDate } from 'class-validator';
 export class CreateBookDto {
   @IsString()
   @IsNotEmpty()
-  title: string;
+  title: string;            // Titre du livre
 
   @IsDate()
-  publicationDate: Date;
+  publicationDate: Date;    // Date de publication
 
   @IsString()
   @IsNotEmpty()
-  authorId: string;
+  authorId: string;         // ID de l'auteur
 }
